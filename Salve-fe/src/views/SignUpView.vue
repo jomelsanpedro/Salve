d81854ebde5474f15104cc2709fbbf4fc55cc4d4
Jomel San Pedro <jomelsanpedro14@gmail.com>
2023-05-10 22:32:09 +0400

    Making available to sign up

--- Salve-fe/src/views/SignUpView.vue
+++ Salve-fe/src/views/SignUpView.vue
@@ -2,10 +2,11 @@
     <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div class="main-left">
             <div class="p-12 bg-slate-700 rounded-lg">
-                <form class="space-y-8">
+                <form class="space-y-8" v-on:submit.prevent="submitForm">
                     <div class="relative">
                         <input 
-                            type="text" 
+                            type="text"
+                            v-model="form.name" 
                             placeholder="Your full name" 
                             class="peer w-full mt-2 py-4 px-3 border border-gray-200 rounded-lg placeholder-transparent">
                         <label class="
@@ -27,7 +28,8 @@
 
                     <div class="relative">
                         <input
-                            type="email" 
+                            type="email"
+                            v-model="form.email" 
                             placeholder="Your e-mail address" 
                             class="peer w-full mt-2 py-4 px-3 border border-gray-200 rounded-lg placeholder-transparent">
                         <label class="
@@ -49,7 +51,8 @@
 
                     <div class="relative">
                         <input 
-                            type="password" 
+                            type="password"
+                            v-model="form.password1" 
                             placeholder="Your password" 
                             class="peer w-full mt-2 py-4 px-3 border border-gray-200 rounded-lg placeholder-transparent">
                         <label class="
@@ -71,7 +74,8 @@
 
                     <div class="relative">
                         <input 
-                            type="password" 
+                            type="password"
+                            v-model="form.password2" 
                             placeholder="Repeat your password" 
                             class="peer w-full mt-2 py-4 px-3 border border-gray-200 rounded-lg placeholder-transparent">
                         <label class="
@@ -90,6 +94,11 @@
                             pointer-events-none
                             ">Repeat password</label><br>
                     </div>
+                    <template v-if="errors.length > 0">
+                        <div class="bg-red-300 text-white rounded-lg p-6">
+                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                        </div>
+                    </template>
 
                     <div>
                         <button class="py-4 px-6 bg-blue-500 text-slate-100 rounded-lg">Sign up</button>
@@ -111,11 +120,75 @@
 </template>
 <script>
 import Intro from '../components/Intro.vue'
+import { useToastStore } from '@/stores/toast'
+import axios from 'axios'
 
 export default {
-    name: 'FeedView',
+    name: 'SignUpView',
     components: {
         Intro,
+    },
+
+    setup() {
+        const toastStore = useToastStore()
+
+        return {
+            toastStore
+        }
+    },
+
+    data() {
+        return {
+            form: {
+                email: '',
+                name: '',
+                password1: '',
+                password2: ''
+            },
+            errors: [],
+        }
+    },
+
+    methods: {
+        submitForm() {
+            this.errors = []
+
+            if (this.form.email === '') {
+                this.errors.push('Your e-mail is missing')
+            }
+
+            if (this.form.name === '') {
+                this.errors.push('Your name is missing')
+            }
+
+            if (this.form.password1 === '') {
+                this.errors.push('Your password is missing')
+            }
+
+            if (this.form.password1 !== this.form.password2) {
+                this.errors.push('The password does not match')
+            }
+
+            if (this.errors.length === 0) {
+                axios
+                    .post('/api/signup/', this.form)
+                    .then(response => {
+                        if (response.data.message === 'success') {
+                            this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
+
+                            this.form.email = ''
+                            this.form.name = ''
+                            this.form.password1 = ''
+                            this.form.password2 = ''
+                        } else {
+                            this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-red-300')
+                        }
+                    })
+                    .catch(error => {
+                        console.log('error', error)
+                    })
+            }
+        }
     }
 }
 </script>
--- SalveBE/account/urls.py
+++ SalveBE/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from . import api
 
 urlpatterns = [
+    path('singup/', api.signup, name='singup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
